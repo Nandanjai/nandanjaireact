f67bf4967bcf480334490c2b3ee2685cd6db6a94
nandanjai <nandannadujai@gmail.com>
2023-11-17 12:39:50 +0530

    frist comit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Body from './components/Body';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Body></Body>
+    </div>
+      
+  );
+}
+
+export default App;
